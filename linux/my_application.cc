f18ee96d66f38bed6bcd368e9cbf83169bf3644c
domohuhn <46851472+domohuhn@users.noreply.github.com>
2024-05-24 22:08:31 +0200

    fix: rename function call

--- linux/my_application.cc
+++ linux/my_application.cc
@@ -21,7 +21,7 @@ static void my_application_activate(GApplication* application) {
       GTK_WINDOW(gtk_application_window_new(GTK_APPLICATION(application)));
 
   // set icon 
-  auto icon_theme = gtk_icon_theme_get_for_display(gdk_display_get_default());
+  auto icon_theme = gtk_icon_theme_get_for_screen(gdk_display_get_default());
   gtk_icon_theme_add_search_path(icon_theme,"data/flutter_assets/assets/images");
   const char* icon = "flutter_logo";
   if (gtk_icon_theme_has_icon(icon_theme,icon)==1) {
